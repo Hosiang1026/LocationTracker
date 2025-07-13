658d1e51b1a3bdac5b36f9d13aa01c4398f2c774
root <hosiang1026@sina.com>
2025-07-13 12:21:20 +0800

    update apk

---
+++ app/build/generated/source/buildConfig/release/com/ljs/locationtracker/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ljs.locationtracker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.hx.locationtracker";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "2.1.4";
+  // Field from default config.
+  public static final String CONFIG_URL = "https://ha.haoxiang.eu.org/locationtracker/api/config";
+  // Field from default config.
+  public static final String HEARTBEAT_URL = "https://ha.haoxiang.eu.org/locationtracker/api/config/heartbeat";
+  // Field from default config.
+  public static final String WEBHOOK_URL = "https://ha.haoxiang.eu.org/api/webhook/8612f073252c388571fc029559caadd3b5336db3aaec7d98be698e7e1e236a83";
+}
